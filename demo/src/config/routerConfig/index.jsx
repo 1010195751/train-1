a423845b81a439a59a1565138037c471a7600214
A87108957 <871089057@qq.com>
2020-06-11 18:34:12 +0800

    test

--- demo/src/config/routerConfig/index.jsx
+++ demo/src/config/routerConfig/index.jsx
@@ -1,5 +1,6 @@
 import Home from '../../page/Home';
-
+import About from '../../page/About';
 export const RouterConfig = [
-    { path: '/', exact: true, component: Home }
+    { path: '/', exact: true, component: Home },
+    { path: '/about', exact: true, component: About }
 ]
--- demo/src/page/Home/index.jsx
+++ demo/src/page/Home/index.jsx
@@ -1,17 +1,19 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import { Link } from 'react-router-dom';
 import './home.scss';
 function Home(){
 
+    const [ data, setData ] = useState();
+
     useEffect(()=>{
         fetchData();
-    },[])
+    },[  ])
 
     const fetchData = ()=>{
         axios.get('http://rap2.taobao.org:38080/app/mock/243407/test/2020128195616')
         .then((res)=>{
-            console.log('成功获取');
-            console.log(res);
+            setData(res.data);
         },(error)=>{
             console.log('获取失败');
             console.log(error);
@@ -20,7 +22,13 @@ function Home(){
 
     return (
         <div id = "home-main-div">
-
+            Home {
+                data !== undefined ? data.list : ""
+            }
+            <Link 
+                to = "/about"
+            > About
+            </Link>
         </div>
     )
 }
